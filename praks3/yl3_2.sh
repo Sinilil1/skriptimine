897f882ea1674b56c2ab286574f432fb14e94349
Richard Ritso <richard.ritso@voco.ee>
2026-09-04 06:35:29 -0400

    lisatud kommentaarid praks 3 yl3_2

--- praks3/yl3_2.sh
+++ praks3/yl3_2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+#Alternatiivne viis kuidas lahendada  praks 3 yl 3 lahendamiseks, mis on lühem
+
+
+#sisendid
 read -p "Reisijate arv: " rarv
 read -p "Kohtade arv bussis: " karv
 
+
+#jagab ilma , kohata reisiad ja % leiab jäägi.
 echo "Täitunud busse on $(($rarv / $karv)) ja ülejäänud reisijaid on $(($rarv % $karv))"
